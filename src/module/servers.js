8b3f5072fe80a217447147ec9a85e444761bc083
liuhan <liuhann@gmail.com>
2018-06-15 10:45:48 +0800

    ref

--- src/module/servers.js
+++ src/module/servers.js
@@ -1,14 +1,15 @@
-import * as axios from 'axios';
+import * as axios from 'axios'
 
 export default {
-  async onload(ctx, next) {
+  async load (ctx) {
     ctx.clients = {}
     if (ctx.bootOpts.servers) {
-      for(const key in ctx.bootOpts.servers) {
-        //each server has different config
-        ctx.clients[key] = axios.create(Object.assign({
-          withCredentials: true
-        }, ctx.bootOpts.servers[key]));
+      for (const key in ctx.bootOpts.servers) {
+        // multi-client
+        ctx.clients[key] = axios.create({
+          withCredentials: true,
+          ...ctx.bootOpts.servers[key]
+        })
       }
     }
     ctx.client = ctx.clients.default || axios.create(Object.assign({
